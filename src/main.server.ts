6260ba6b25b8b86725e076cf9d0c36308eebc89f
Jainam9261 <shahjainam9261@gmail.com>
2026-01-12 01:36:05 +0530

    Remove SSR files and fix Angular static build for Vercel

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-export default function bootstrap(context: BootstrapContext) {
-  return bootstrapApplication(AppComponent, config, context);
-}
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
